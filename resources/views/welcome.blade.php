3dbcbd8a6bd2cef453b7dd8fdc55423a5d00d0cb
rifkiazizi <47319509+rifkiazizi@users.noreply.github.com>
2021-04-15 14:42:23 +0700

    tugas

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title> Rifki </title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #d9d9d9;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+
+            .template1 {
+
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+<br><br>
+            <div class="content">
+                <div class="title m-b-md">
+                    BIODATA SAYA
+                </div>
+               
+
+
+
+
+<form action="#" style="width: 1000px"class="posisi";>
+<fieldset class="h"/>
+<table style="width: 980px;">
+<tr>
+<td rowspan="15" width="250px">
+<img src="{{ ('img/rifki.jpg') }}" width="200px" height="220px"/>
+<td><b> 18090020 <b></p>
+<p> Rifki Azizi </p>
+<p> 6A </p>
+</table>
+</fieldset>
+</form>
+
+    </div>
+            </div>
+        </div>
+    </body>
+    <br><br>
+</html>
